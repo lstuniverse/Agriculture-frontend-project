25f5a2e376ca6382ceb94c255a28551ad906f6c0
lstuniverse <lst.wpm@gmail.com>
2018-08-11 16:16:52 +0700

    update-login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './layouts/login/login.component';
 import { HomeComponent } from './layouts/home/home.component';
+import { RegisterComponent } from './layouts/register/register.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
